0cda942744a423aa6b262bfa4f3955c6e29ff141
David De Ridder <david@Davids-MacBook-Pro.local>
2018-09-11 23:10:53 +0200

    my first git commit

--- createPopup.py
+++
@@ -1,7 +0,0 @@
-def createPopupText (col_names):
-    """Return popup message for Folium markers"""
-    popup_text = """"""
-    for name in col_names:
-        format_name = name + ': ' + '{} ' + '<br> '
-        popup_text += format_name
-    return popup_text
